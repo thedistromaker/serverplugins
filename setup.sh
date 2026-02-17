8ab44f20b0a5d24bd5fc6331e73fbadccc0eeeea
thedistromaker <pan.savvy.35@gmail.com>
2026-02-17 11:06:45 +0000

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ $EUID -ne 0 ]; then
+  echo "E: Please run this script as root."
+  exit 1
+fi
+mkdir -pv /opt/pluginmanager/tmp /opt/pluginmanager/config /opt/pluginmanager/downloads /opt/pluginmanager/logs
+read -r " -> Please enter your server version:" sver
+echo $sver
+echo $sver > /opt/pluginmanager/config/server-version.txt
+echo " -> Downloading colours configuration..."
+wget -q -O /opt/pluginmanager/config/colours.conf https://raw.githubusercontent.com/thedistromaker/serverplugins/setuptools/colours.conf
+echo " -> Downloading version configuration..."
+wget -q -O /opt/pluginmanager/config/version.txt https://raw.githubusercontent.com/thedistromaker/serverplugins/main/version.txt
+echo " -> Downloading and setting up script..."
+wget -q -O /usr/bin/pluginmgr https://raw.githubusercontent.com/thedistromaker/serverplugins/setuptools/script.sh
+chmod +x /usr/bin/pluginmgr
+echo " -> Done!"
+exit 0
